2049635c4092643c8c4051e96081ce5a509f45a5
lunadis <luislopes116@outlook.com>
2020-07-08 21:24:54 -0300

    commit day +1

--- api/src/Facade/Interfaces/IProjectsAplication.ts
+++ api/src/Facade/Interfaces/IProjectsAplication.ts
@@ -1,13 +1,13 @@
 import { Projects } from "../../Domain/models/Projects";
 import IProjectsRepositories from "../../Domain/Repositories/IProjectsRepositories";
 
-interface IProjectRepositorie{
+interface IProjecta{
 
     Save(entity: Projects): boolean
     Load(id: number): Projects
     Delete(id: number): Projects
     Index(): Array<Projects>
-    
+
 }
 
 export default IProjectsRepositories
